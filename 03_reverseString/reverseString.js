748d34d12732771c30279d60e52d8e72fe2bc40d
dvh <177888353+dvhlearning@users.noreply.github.com>
2024-12-13 17:03:06 -0500

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseString = "";
+    for (let index = string.length-1; index >= 0; index--) {
+        reverseString += string[index];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
