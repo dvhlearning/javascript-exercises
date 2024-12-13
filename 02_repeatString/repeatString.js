cff5407541d039ab07f949e8ae1ca1f25aaa5bc4
dvh <177888353+dvhlearning@users.noreply.github.com>
2024-12-13 16:54:43 -0500

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        let message = "";
+        for (let index = 0; index < num; index++) {
+            message += string;
+        }
+        return message;
+    }
 };
 
 // Do not edit below this line
