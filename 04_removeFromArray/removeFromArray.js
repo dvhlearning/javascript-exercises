396abd216b4448bffd398fe6933df3970c4a4f8c
dvh <177888353+dvhlearning@users.noreply.github.com>
2024-12-17 18:45:26 -0500

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeArr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < removeArr.length; j++) {
+            if (arr[i] === removeArr[j]){
+                arr.splice(i,1);
+                i--;
+                console.log(arr);
+            }
+        }
+    }
+    return arr;
 };
+
+console.log(removeFromArray([1,2,3,4],3,2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
