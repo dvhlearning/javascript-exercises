092df1868c5c334a81fb96e52aef02f8d9d8960d
dvh <177888353+dvhlearning@users.noreply.github.com>
2024-12-17 19:11:42 -0500

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let cTemp = (fTemp - 32) * (5/9);
+  let roundedCTemp = Math.round(cTemp * 10) / 10;
+  return roundedCTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fTemp = cTemp * (9/5) + 32;
+  let roundedFTemp = Math.round(fTemp * 10) / 10;
+  return roundedFTemp;
 };
 
 // Do not edit below this line
