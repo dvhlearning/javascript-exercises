39660a822480c66f6a6d0c3d1f455c78c82532c7
dvh <177888353+dvhlearning@users.noreply.github.com>
2024-12-17 19:35:46 -0500

    missed exercise 5 (did 6 instead). All good now! Exercises 1-7 are complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (!checkNum(num1) || !checkNum(num2)){
+        return "ERROR";
+    }
+    let index = 0;
+    let target = 0;
 
+    if (num2 > num1) {
+        index = num1;
+        target = num2;
+    } else {
+        index = num2;
+        target = num1;
+    }
+    
+    let sum = 0;
+    while (index <= target){
+        sum += index;
+        index++;
+    }
+    return sum;
 };
+
+function checkNum(num){
+    if (Number.isInteger(num) && num > 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(sumAll(4,1));
 
 // Do not edit below this line
 module.exports = sumAll;
