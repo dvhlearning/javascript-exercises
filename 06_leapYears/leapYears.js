b9785bedf7b58f3dff61d1fd7e8d857005b4990c
dvh <177888353+dvhlearning@users.noreply.github.com>
2024-12-17 19:01:08 -0500

    completed exercise 5

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0){
+        console.log("divisible by four");
+        if ((year % 100 === 0) && (year % 400 != 0)){
+            console.log("divisible by 100 but NOT by 400");
+            return false;
+        }
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
